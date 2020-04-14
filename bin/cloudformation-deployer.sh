3a35126b727706578bd019934a4ad413ad2dbdea
Jason Harris <jiharris90@gmail.com>
2020-04-14 14:50:58 +0100

    Update cloudformation-deployer.sh

--- bin/cloudformation-deployer.sh
+++ bin/cloudformation-deployer.sh
@@ -10,6 +10,8 @@
 
 . ./logging.sh
 
+cd ${GITHUB_WORKSPACE}/cloudformation-templates
+
 deploy (){
     einfo "Deploying: ${1}"
     aws cloudformation deploy --template-file ${1} --stack-name ${2} --capabilities CAPABILITY_NAMED_IAM --no-fail-on-empty-changeset
